8cac7e4661e7030a8ebb3faebce8102289453624
Enrico Fusto <rikettsie@gmail.com>
2015-04-08 11:31:50 +0200

    fixed migration for change name type

--- db/migrate/6_change_tag_name_type.rb
+++ db/migrate/6_change_tag_name_type.rb
@@ -1,9 +1,13 @@
 class ChangeTagNameType < ActiveRecord::Migration
   def up
+    remove_index :tags, :name
   	change_column :tags, :name, :text
+    add_index :tags, :name, unique: true
   end
 
   def down
+    remove_index :tags, :name
   	change_column :tags, :name, :string
+    add_index :tags, :name, unique: true
   end
 end
